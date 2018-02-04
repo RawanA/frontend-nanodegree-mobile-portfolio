b7616f8d41b60bbb1401ebc4a074e3bd63772d74
RawanA <rawan.abdullah.bs@hotmail.com>
2018-02-04 03:54:43 +0300

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -507,10 +507,11 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  var phase ;
   for (var i = 0; i < items.length; i++) {
     // document.body.scrollTop is no longer supported in Chrome.
-    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var phase = Math.sin((scrollTop / 1250) + (i % 5));
+    phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
